1e1a352678f9e21017c31e2be9b43ba24bcf158c
Akim van Eersel <vaneerselakim@protonmail.com>
2021-02-24 18:10:51 +0100

    Setup new venv

--- standalone-py-scripts/tweets.py
+++ standalone-py-scripts/tweets.py
@@ -1,10 +1,11 @@
 import tweepy
+import os
 
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler("",                         # CONSUMER_KEY
-    "")                       # CONSUMER_SECRET
-auth.set_access_token("966718127076708352-",     # ACCESS_TOKEN
-    "")                            # ACCESS_TOKEN_SECRET
+auth = tweepy.OAuthHandler(os.getenv("user_api"),                         # CONSUMER_KEY
+    os.getenv("user_key"))                       # CONSUMER_SECRET
+auth.set_access_token(os.getenv("content_api"),     # ACCESS_TOKEN
+    os.getenv("content_key"))                            # ACCESS_TOKEN_SECRET
 
 api = tweepy.API(auth)
 
@@ -18,7 +19,7 @@ timeline = api.home_timeline()
 for tweet in timeline:
     print(f"{tweet.user.name} said {tweet.text}")
 
-#api.update_status("Test tweet from Tweepy Python")
+api.update_status("Test tweet from Tweepy Python")
 
 
 
