749fd71dd8613480f130838cd0dd6d9b8334bddd
Akim van Eersel <vaneerselakim@protonmail.com>
2021-02-24 12:50:47 +0100

    Change keys

--- standalone-py-scripts/tweets.py
+++ standalone-py-scripts/tweets.py
@@ -1,10 +1,10 @@
 import tweepy
 
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler("FRjTrMdRfIs22mjJFzOAFesCl",                         # CONSUMER_KEY
-    "dKT7iC2UfvtLPDifBOrxksycWfG3S6Qm7vSBmHprwKFr524Jp5")                       # CONSUMER_SECRET
-auth.set_access_token("966718127076708352-tQc9kNqiRrV6shJow6wHTOBsyNIVFnX",     # ACCESS_TOKEN
-    "Bwice4HoTmeuTlXBQLRjrfzmW6lvQmyhEOreq4jjritL4")                            # ACCESS_TOKEN_SECRET
+auth = tweepy.OAuthHandler("",                         # CONSUMER_KEY
+    "")                       # CONSUMER_SECRET
+auth.set_access_token("966718127076708352-",     # ACCESS_TOKEN
+    "")                            # ACCESS_TOKEN_SECRET
 
 api = tweepy.API(auth)
 
